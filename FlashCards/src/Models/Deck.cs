a7dc182ae84b5c63a8ba6e7e7f44b28f3b1819db
George Everett <gjemba@hotmail.com>
2021-02-02 01:21:21 +0000

    Create base Deck and Card models

---
+++ FlashCards/src/Models/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace src.Models
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
